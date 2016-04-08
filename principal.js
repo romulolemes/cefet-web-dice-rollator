1ed6e44c61026673084128668f0243eb5b2f49e7
Romulo Lemes <romulolemes01@hotmail.com>
2016-04-07 23:03:35 -0300

    Finalizando Exercicio

--- principal.js
+++ principal.js
@@ -11,4 +11,32 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var botaoRolar = document.getElementById('rolar');
+var recipienteResultados = document.getElementById('recipienteResultados');
+var resultado = document.getElementById('resultado');
+var todasAsQtdes = [4,6,8,10,12,20];
 
+botaoRolar.addEventListener('click', function(e) {
+	console.clear();
+	recipienteResultados.classList.remove('oculto');
+	var idElement = 'quantidadeD';
+	var somatorio = 0;
+	var resultString = '';
+	for (var i in todasAsQtdes) {
+		var dadoDX = document.getElementById(idElement.concat(todasAsQtdes[i]));
+		console.log('Quantidade'.concat(i));
+		for (var j=0; j < dadoDX.value; j++){
+			var resulAux = Math.ceil(Math.random() * todasAsQtdes[i]);
+			resultString = resultString.concat(resulAux);
+			resultString = resultString.concat(' ');
+			resultString = resultString.concat('+');
+			resultString = resultString.concat(' ');
+			somatorio += resulAux;
+		}
+	}
+	resultString = resultString.substring(0,(resultString.length-2));
+	resultString = resultString.concat('= ');
+	resultString = resultString.concat(somatorio);
+	resultado.innerHTML = resultString;
+
+});
